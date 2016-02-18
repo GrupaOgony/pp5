6298a145d193b30b765f0962e6c67e0074d5fd25
Kamil Walas <k_walas@poczta.pl>
2016-02-19 00:07:03 +0100

    dodanie mechanizmu rezerwacji ktory dziala, dodawane sa rekordy do bazy danych, platnosci dotpay nie dzialaja

--- src/lanoseoBundle/Controller/DefaultController.php
+++ src/lanoseoBundle/Controller/DefaultController.php
@@ -2,9 +2,13 @@
 
 namespace lanoseoBundle\Controller;
 
+use lanoseoBundle\Entity\Customers;
+use lanoseoBundle\Entity\Orders;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class DefaultController extends Controller
 {
@@ -83,23 +87,56 @@ class DefaultController extends Controller
 
     public function summaryAction(Request $request)
     {
+
+
         $place = $request ->request->get('place');
         $fromDate = $request ->request->get('fromDate');
         $toDate = $request ->request->get('toDate');
-
         $carId = $request->get('carId');
         $repository = $this->getDoctrine()->getRepository('lanoseoBundle:Cars');
-
         $car = $repository->findBy(array('carId' => $carId ));
+
+
+        $customerRepo = $this ->getDoctrine()->getRepository('lanoseoBundle:Customers');
+        $customer = $customerRepo->findAll();
+        $selectedCustomer = $customer[0];
+
+        $nowDate = new \DateTime();
+        $fromDateOrder = \DateTime::createFromFormat('m/d/Y g:i A', $fromDate);
+        $toDateOrder = \DateTime::createFromFormat('m/d/Y g:i A', $toDate);
+
+        $order = new Orders();
+        $order -> setOrderDate($nowDate);
+        $order -> setOrderFrom($fromDateOrder);
+        $order -> setOrderTo($toDateOrder);
+        $order -> setOrderPayment(false);
+        $order -> setOrderPrice(153);
+        $order -> setCustomer($selectedCustomer);
+        $order -> setCar($car[0]);
+//dodawanie rekordów!
+//        $em = $this->getDoctrine()->getManager();
+//        $em->persist($order);
+//        $em->flush();
+
+        $me = $this -> getDoctrine() -> getEntityManager();
+        $con = $me->getConnection();
+        $query = $con->prepare("SELECT order_id FROM orders ORDER BY order_id DESC LIMIT 1");
+        $query->execute();
+        $result = $query->fetchAll();
+
+
+
         return $this->render('lanoseoBundle:Default:summary.html.twig', array(
 
             'place' => $place,
             'fromDate' => $fromDate,
             'toDate' => $toDate,
             'car' => $car,
+            'result' => $result,
 
         ));
     }
+
 
 
 }
