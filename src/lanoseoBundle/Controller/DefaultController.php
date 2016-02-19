94d663182fabb32cb05067bfb70b46ae26b8c855
Pawel Rachwal <e123@onet.pl>
2016-02-19 12:20:53 +0100

    poprawiona rezerwacja samochodu, dodane wykluczane samochodów na liście

--- src/lanoseoBundle/Controller/DefaultController.php
+++ src/lanoseoBundle/Controller/DefaultController.php
@@ -4,11 +4,13 @@ namespace lanoseoBundle\Controller;
 
 use lanoseoBundle\Entity\Customers;
 use lanoseoBundle\Entity\Orders;
+use lanoseoBundle\Entity\Cars;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints\Date;
 use Symfony\Component\Validator\Constraints\DateTime;
+
 
 class DefaultController extends Controller
 {
@@ -45,6 +47,15 @@ class DefaultController extends Controller
     }
     public function carlistAction()
     {
+        $nowDate = new \DateTime();
+        $me = $this -> getDoctrine() -> getEntityManager();
+        $con = $me->getConnection();
+        $query = $con->prepare("SELECT car_id, order_payment, order_to FROM orders WHERE order_to <".$nowDate->format('YYYY-mm-dd'));
+        $query->execute();
+        $result = $query->fetchAll();
+
+
+
         $repository = $this->getDoctrine()->getRepository('lanoseoBundle:Cars');
 
         $cars = $repository->findAll();
@@ -52,6 +63,8 @@ class DefaultController extends Controller
         return $this->render('lanoseoBundle:Default:carlist.html.twig', array(
 
             'cars' => $cars,
+            'result' => $result,
+
 
         ));
     }
@@ -114,9 +127,9 @@ class DefaultController extends Controller
         $order -> setCustomer($selectedCustomer);
         $order -> setCar($car[0]);
 //dodawanie rekordów!
-//        $em = $this->getDoctrine()->getManager();
-//        $em->persist($order);
-//        $em->flush();
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($order);
+        $em->flush();
 
         $me = $this -> getDoctrine() -> getEntityManager();
         $con = $me->getConnection();
@@ -125,14 +138,60 @@ class DefaultController extends Controller
         $result = $query->fetchAll();
 
 
+        //obliczanie ceny
+
+        $diff = $fromDateOrder->diff($toDateOrder);
+        $price = 0;
+        $discount = 0;
+        $percentageDiscount = 0;
+
+
+
+        $query = $con->prepare("SELECT order_id FROM orders ORDER BY order_id DESC LIMIT 1");
+        $query->execute();
+        $result = $query->fetchAll();
+
+        $anotherQuery = $con->prepare("SELECT COUNT(customer_id) FROM `orders` WHERE customer_id=".$selectedCustomer->getCustomerId());
+        $anotherQuery->execute();
+        $otherResult = $anotherQuery->fetchAll();
+
+        if ($otherResult[0]["COUNT(customer_id)"] >= 3)
+        {
+            $price = $diff->d * $car[0]->getCarPrice() * 0.8;
+            $discount = $diff->d * $car[0]->getCarPrice() * 0.2;
+            $percentageDiscount = 20;
+
+        }
+        else{
+            if ($diff->d > 7)
+            {
+                $price = $diff->d * $car[0]->getCarPrice() * 0.9;
+                $discount = $diff->d * $car[0]->getCarPrice() * 0.1;
+                $percentageDiscount = 10;
+            }
+            else
+            {
+                $price = $diff->d * $car[0]->getCarPrice();
+                $discount = $diff->d * $car[0]->getCarPrice() * 0;
+                $percentageDiscount = 0;
+            }
+        }
+
+
+
 
         return $this->render('lanoseoBundle:Default:summary.html.twig', array(
 
+            'percentageDiscount' => $percentageDiscount,
+            'discount' => $discount,
             'place' => $place,
             'fromDate' => $fromDate,
             'toDate' => $toDate,
             'car' => $car,
             'result' => $result,
+            'price' => $price,
+            'customer' => $selectedCustomer,
+
 
         ));
     }
